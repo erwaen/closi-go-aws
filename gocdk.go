9b584bb8b065b2ede5724ca8e2940450679caa24
erik <erikwasmosy98@gmail.com>
2024-05-15 19:45:00 -0400

    decouple db

--- gocdk.go
+++ gocdk.go
@@ -20,22 +20,22 @@ func NewGocdkStack(scope constructs.Construct, id string, props *GocdkStackProps
 	}
 	stack := awscdk.NewStack(scope, &id, &sprops)
 
-    // create DB table here
-    table := awsdynamodb.NewTable(stack, jsii.String("myUserTable"), &awsdynamodb.TableProps{
-        PartitionKey: &awsdynamodb.Attribute{
-            Name: jsii.String("username"),
-            Type: awsdynamodb.AttributeType_STRING,
-        },
-        TableName: jsii.String("userTable"),
-    })
-
-    myFunction := awslambda.NewFunction(stack, jsii.String("myLambdsaFunction"), &awslambda.FunctionProps{
-		Runtime: awslambda.Runtime_PROVIDED_AL2023(),
-		Code:    awslambda.AssetCode_FromAsset(jsii.String("lambda/function.zip"), nil),
-        Handler: jsii.String("main"),
+	// create DB table here
+	table := awsdynamodb.NewTable(stack, jsii.String("myUserTable"), &awsdynamodb.TableProps{
+		PartitionKey: &awsdynamodb.Attribute{
+			Name: jsii.String("username"),
+			Type: awsdynamodb.AttributeType_STRING,
+		},
+		TableName: jsii.String("userTable"),
 	})
 
-    table.GrantReadWriteData(myFunction)
+	myFunction := awslambda.NewFunction(stack, jsii.String("myLambdaFunction"), &awslambda.FunctionProps{
+		Runtime: awslambda.Runtime_PROVIDED_AL2023(),
+		Code:    awslambda.AssetCode_FromAsset(jsii.String("lambda/function.zip"), nil),
+		Handler: jsii.String("main"),
+	})
+
+	table.GrantReadWriteData(myFunction)
 
 	return stack
 }
